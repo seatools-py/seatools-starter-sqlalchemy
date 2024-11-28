1fd754c5196311e1fbdb38d2c0321cb4ea068201
dragons96 <jinqunlong@black-unique.com>
2024-11-28 18:23:22 +0800

    release: 1.0.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=requirements,
     extras_require={},
     license="MIT license",
-    long_description=readme,
+    long_description='',
     include_package_data=True,
     keywords=['seatools', 'ioc', 'starter', 'sqlalchemy'],
     name='seatools-starter-sqlalchemy',
