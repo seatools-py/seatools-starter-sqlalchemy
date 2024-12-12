2a7c17f1923bf90cd30489730b4a1ee8e1e4f965
dragons96 <jinqunlong@black-unique.com>
2024-12-12 14:15:10 +0800

    release: 1.0.4

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitee.com/seatools-py/seatools-starter-sqlalchemy',
-    version='1.0.3',
+    version='1.0.4',
     zip_safe=False,
 )
